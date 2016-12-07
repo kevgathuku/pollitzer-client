e1076354fe62c1419800a7fcfdb058d17daea045
Kevin Ndung'u <kevgathuku@gmail.com>
2016-12-07 19:50:33 +0300

    Implement root App route with react-router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Router, browserHistory} from 'react-router';
+
+import App from './components/App';
 import Voting from './components/Voting';
+
 import './index.css';
 
-const pair = ['Trainspotting', '28 Days Later'];
-
 ReactDOM.render(
-  <Voting pair={pair}/>,
+  <Router history={browserHistory}>
+    <Route component={App}>
+      <Route path="/" component={Voting} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
