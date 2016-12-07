033b501cc043ad31d1ddd0d952f19248b3660185
Kevin Ndung'u <kevgathuku@gmail.com>
2016-12-07 10:57:29 +0300

    Add tests for rendering Voting component

--- src/components/Voting.test.js
+++ src/components/Voting.test.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag
+} from 'react-addons-test-utils';
+import { expect } from 'chai';
+
 import Voting from './Voting';
 
 describe('Voting', () => {
@@ -7,5 +13,16 @@ describe('Voting', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Voting />, div);
   });
+
+   it('renders a pair of buttons', () => {
+     const component = renderIntoDocument(
+        <Voting pair={["Trainspotting", "28 Days Later"]} />
+      );
+     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].textContent).to.equal('Trainspotting');
+    expect(buttons[1].textContent).to.equal('28 Days Later');
+   });
 
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,6 @@
 import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme'
 import chaiImmutable from 'chai-immutable';
 
 chai.use(chaiImmutable);
+chai.use(chaiEnzyme());
