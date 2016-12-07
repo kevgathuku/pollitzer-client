50f557bff3075c78c95e11591d52b339737e4095
Kevin Ndung'u <kevgathuku@gmail.com>
2016-12-07 20:05:03 +0300

    Implement winners view when results are tallied
    Either render the results or the winner screen when voting is done

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 
+import Winner from './Winner';
+
 class Results extends PureComponent {
   getPair() {
     return this.props.pair || [];
@@ -13,15 +15,26 @@ class Results extends PureComponent {
   }
 
   render() {
-    return <div className="results">
-       {this.getPair().map(entry =>
-         <div key={entry} className="entry">
-           <h1>{entry}</h1>
-           <div className="voteCount">
-            {this.getVotes(entry)}
-          </div>
-         </div>
-       )}
+    return this.props.winner ?
+      <Winner ref="winner" winner={this.props.winner} /> :
+      <div className="results">
+        <div className="tally">
+         {this.getPair().map(entry =>
+           <div key={entry} className="entry">
+             <h1>{entry}</h1>
+             <div className="voteCount">
+              {this.getVotes(entry)}
+            </div>
+           </div>
+         )}
+        </div>
+        <div className="management">
+          <button ref="next"
+                  className="next"
+                  onClick={this.props.next}>
+            Next
+          </button>
+        </div>
      </div>;
  }
 }
