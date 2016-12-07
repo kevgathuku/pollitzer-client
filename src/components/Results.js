e99a6136fc7fa2b6359a0c366cfeffc936a4cef3
Kevin Ndung'u <kevgathuku@gmail.com>
2016-12-07 19:58:02 +0300

    Implement Results component

---
+++ src/components/Results.js
@@ -0,0 +1,29 @@
+import React, { PureComponent } from 'react';
+
+class Results extends PureComponent {
+  getPair() {
+    return this.props.pair || [];
+  }
+
+  getVotes(entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  }
+
+  render() {
+    return <div className="results">
+       {this.getPair().map(entry =>
+         <div key={entry} className="entry">
+           <h1>{entry}</h1>
+           <div className="voteCount">
+            {this.getVotes(entry)}
+          </div>
+         </div>
+       )}
+     </div>;
+ }
+}
+
+export default Results;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Route, Router, browserHistory} from 'react-router';
 
 import App from './components/App';
+import Results from './components/Results';
 import Voting from './components/Voting';
 
 import './index.css';
@@ -11,6 +12,7 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route component={App}>
       <Route path="/" component={Voting} />
+      <Route path="/results" component={Results} />
     </Route>
   </Router>,
   document.getElementById('root')
